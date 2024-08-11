83487e531e5c5a0bb0da709ddfd9db1c10ad039c
Alexandr Muhametchin <devilmaker228@gmail.com>
2024-08-11 19:24:11 +0700

    Create Proj

--- mailing_project/mailings/forms.py
+++ mailing_project/mailings/forms.py
@@ -1,7 +1,8 @@
 from django import forms
 from .models import Mailing
 
+
 class MailingForm(forms.ModelForm):
     class Meta:
         model = Mailing
-        fields = ['title', 'message', 'send_time', 'status', 'recipients']
+        fields = ["title", "message", "send_time", "status", "recipients"]
