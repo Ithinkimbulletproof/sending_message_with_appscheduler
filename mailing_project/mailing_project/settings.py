9e0d862d89c4ea5a73a79478b84b9f2c89817104
Alexandr Muhametchin <devilmaker228@gmail.com>
2024-08-11 19:26:06 +0700

    Create Proj

--- mailing_project/mailing_project/settings.py
+++ mailing_project/mailing_project/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     "mailings.apps.MailingsConfig",
-    "users.apps.UsersConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
