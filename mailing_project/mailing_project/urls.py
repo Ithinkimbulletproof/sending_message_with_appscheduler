b91f57c8d3ae036de906e6f5478f1e7b927fbb37
Alexandr Muhametchin <devilmaker228@gmail.com>
2024-08-12 16:11:33 +0700

    Create Users

--- mailing_project/mailing_project/urls.py
+++ mailing_project/mailing_project/urls.py
@@ -1,9 +1,8 @@
+from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import RedirectView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('mailings/', RedirectView.as_view(url='mailings/mailings/', permanent=False)),
-    path('mailings/mailings/', include('mailings.urls')),
-    path('users/', include('users.urls')),
+    path("admin/", admin.site.urls),
+    path("mailings/", include("mailings.urls")),
+    path("users/", include("users.urls")),
 ]
