b4d47fff06c22521f0d953d3d7d11e5b443729da
Alexandr Muhametchin <devilmaker228@gmail.com>
2024-08-12 09:41:19 +0700

    Create Users

--- mailing_project/mailing_project/settings.py
+++ mailing_project/mailing_project/settings.py
@@ -10,6 +10,7 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
+    "users"
     "mailings",
     "django_apscheduler",
     "django.contrib.admin",
@@ -100,5 +101,3 @@ APSCHEDULER = {
         },
     ],
 }
-
-AUTH_USER_MODEL = 'users.CustomUser'
