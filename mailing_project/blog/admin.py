fb119dfc4c4a2b3d6baeb345a365eb740f87fa6e
Alexandr Muhametchin <devilmaker228@gmail.com>
2024-08-15 20:11:12 +0700

    Some fix

--- mailing_project/blog/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import BlogPost
-
-
-@admin.register(BlogPost)
-class BlogPostAdmin(admin.ModelAdmin):
-    list_display = ("title", "publication_date", "views")
-    search_fields = ("title", "content")
--- mailing_project/mailing_project/wsgi.py
+++
@@ -1,8 +0,0 @@
-from django.core.wsgi import get_wsgi_application
-from mailing_project import settings
-from mailings.tasks import start_scheduler
-
-application = get_wsgi_application()
-
-if settings.DEBUG:
-    start_scheduler()
